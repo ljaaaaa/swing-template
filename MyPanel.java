42d200b15985b008aba59276ddd0e0897386b0fd
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-07-07 08:09:57 -0700

    Re-indented to fit vim

--- MyPanel.java
+++ MyPanel.java
@@ -5,15 +5,15 @@ import java.awt.Graphics2D;
 
 public class MyPanel extends JPanel {
 
-    public MyPanel(){
-        setBounds(Constants.FRAME_BOUNDS);
-    }
+    	public MyPanel(){
+        	setBounds(Constants.FRAME_BOUNDS);
+    	}
 
-    @Override
-    protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D)g;
+    	@Override
+    	protected void paintComponent(Graphics g){
+        	super.paintComponent(g);
+        	Graphics2D g2d = (Graphics2D)g;
 
-        g2d.drawImage(new ImageIcon("images/image.png").getImage(), 0, 0, 100, 100, null);
-    }
+        	g2d.drawImage(new ImageIcon("images/image.png").getImage(), 0, 0, 100, 100, null);
+    	}
 }
